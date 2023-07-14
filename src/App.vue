06fd759ea6f0796dbbdefd45d4f8de1a786a80cb
gedons <gedoniani@gmail.com>
2023-07-14 14:34:33 +0100

    App Setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text-2xl font-semibold bg-purple-400">
+      Hello world!
+    </h1>
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
